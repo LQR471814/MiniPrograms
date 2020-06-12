d0626cbf2adaeb6b57335116e265e6e69fd334b4
LQR471814 <bramblefern1013@gmail.com>
2020-06-11 17:52:17 -0700

    Fixed some console compatibility issues

--- Chinese-Pinyin-Symbol-Input.py
+++ Chinese-Pinyin-Symbol-Input.py
@@ -7,26 +7,26 @@ char3 = u"\u030C"
 char4 = u"\u0300"
 charV = u"\u0308"
 
-helpMenu = f"""Chinese Pinyin Symbol Input v0.1.0
-    [ALT] + 1: {char1}
-    [ALT] + 2: {char2}
-    [ALT] + 3: {char3}
-    [ALT] + 4: {char4}
-    [ALT] + v: {charV}
+charList = [char1, char2, char3, char4, charV]
+
+helpMenu = f"""Chinese Pinyin Symbol Input v0.1.1
+    [CTRL] + [ALT] + 1: First Sound
+    [CTRL] + [ALT] + 2: Second Sound
+    [CTRL] + [ALT] + 3: Third Sound
+    [CTRL] + [ALT] + 4: Fourth Sound
+    [CTRL] + [ALT] + u: The u with 2 dots above it
     [CTRL] + q: Quit
 """
-
-charList = [char1, char2, char3, char4, charV]
 
 def hotkeyHandler(keyType):
     global charList
     keyboard.write(charList[keyType], delay=0, restore_state_after=False)
 
-keyboard.add_hotkey("alt+1", hotkeyHandler, args=[0], suppress=True)
-keyboard.add_hotkey("alt+2", hotkeyHandler, args=[1], suppress=True)
-keyboard.add_hotkey("alt+3", hotkeyHandler, args=[2], suppress=True)
-keyboard.add_hotkey("alt+4", hotkeyHandler, args=[3], suppress=True)
-keyboard.add_hotkey("alt+v", hotkeyHandler, args=[4], suppress=True)
+keyboard.add_hotkey("ctrl+alt+1", hotkeyHandler, args=[0], suppress=True)
+keyboard.add_hotkey("ctrl+alt+2", hotkeyHandler, args=[1], suppress=True)
+keyboard.add_hotkey("ctrl+alt+3", hotkeyHandler, args=[2], suppress=True)
+keyboard.add_hotkey("ctrl+alt+4", hotkeyHandler, args=[3], suppress=True)
+keyboard.add_hotkey("ctrl+alt+u", hotkeyHandler, args=[4], suppress=True)
 keyboard.add_hotkey("ctrl+q", sys.exit, args=[0], suppress=True)
 
 print(helpMenu)
